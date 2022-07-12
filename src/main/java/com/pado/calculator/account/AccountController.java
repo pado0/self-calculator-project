3c141f242159c6d8741709dbe25f806981038204
pado0 <bgshhd95@gmail.com>
2022-07-12 15:31:53 +0900

    회원가입 후 인증 context holder 처리

--- src/main/java/com/pado/calculator/account/AccountController.java
+++ src/main/java/com/pado/calculator/account/AccountController.java
@@ -1,14 +1,23 @@
 package com.pado.calculator.account;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +25,12 @@ public class AccountController {
 
     private final AccountRepository accountRepository;
     private final PasswordEncoder passwordEncoder;
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpGet(Model model){
@@ -26,7 +41,11 @@ public class AccountController {
     }
 
     @PostMapping("/sign-up")
-    public String signUpPost(@Valid @ModelAttribute SignUpForm signUpForm) {
+    public String signUpPost(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors) {
+
+        if(errors.hasErrors()){
+            return "/sign-up";
+        }
 
         Account account = Account.builder()
                 .email(signUpForm.getEmail())
@@ -34,6 +53,14 @@ public class AccountController {
                 .build();
 
         accountRepository.save(account);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                account.getEmail(),
+                account.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(token);
 
         return "redirect:/";
     }
