6f04a4e5bea5b1ecee6a2216fd2add3e3cab24df
pado0 <bgshhd95@gmail.com>
2022-07-12 14:57:37 +0900

    회원 가입처리

--- src/main/java/com/pado/calculator/CalculatorApplication.java
+++ src/main/java/com/pado/calculator/CalculatorApplication.java
@@ -2,7 +2,9 @@ package com.pado.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // BaseEntity 사용을 위함
 @SpringBootApplication
 public class CalculatorApplication {
 
