c0bb4badc7a8d4cfd0f4441714d7ef38ca7e3ef6
pado0 <bgshhd95@gmail.com>
2022-07-14 11:15:56 +0900

    페이징 시작

--- src/main/java/com/pado/calculator/exception/ExceptionCode.java
+++ src/main/java/com/pado/calculator/exception/ExceptionCode.java
@@ -3,7 +3,8 @@ package com.pado.calculator.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-    MEMBER_DUPLICATE(404, "Member Email Already exsits");
+    MEMBER_DUPLICATE(404, "Member Email Already exsits"),
+    EXPRESSION_NOT_VALID(404, "입력한 수식이 올바르지 않습니다");
 
     @Getter
     private int status;
