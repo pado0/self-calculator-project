22b835e7faa12cc59a6b6e1905319ac643575dae
pado0 <bgshhd95@gmail.com>
2022-07-15 10:40:30 +0900

    test 수정 및 restdocs 의존성 추가

---
+++ src/test/java/com/pado/calculator/account/AccountRestControllerTest.java
@@ -0,0 +1,56 @@
+package com.pado.calculator.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Test
+    void 회원_가입_성공() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("email", "gywls1743@naver.com")
+                        .param("password", "asdfqwer")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"))
+                .andExpect(authenticated().withUsername("gywls1743@naver.com")); // UsernamePasswordAuthToken 구현 후 동작.
+
+        Account account = accountRepository.findByEmail("gywls1743@naver.com");
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), "asdfqwer");
+
+    }
+    @Test
+    void 회원_가입_입력값오류() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("email", "gywls174xxx")
+                        .param("password", "asdfq")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/sign-up"))
+                .andExpect(unauthenticated()); // UsernamePasswordAuthToken 구현 후 동작.
+
+    }
+
+}
